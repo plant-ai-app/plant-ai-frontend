c4d82e980b3ef9f149fa9bd4ef3dcc0d93c453c2
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-03 12:53:42 -0300

    ✨ :feat: Adiciona novo componente home

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import Container from "../../common/container/Container";
+
+const Home = () => {
+    return (
+        <Container>
+            <h1>Home</h1>
+        </Container>
+    );
+};
+
+export default Home;
