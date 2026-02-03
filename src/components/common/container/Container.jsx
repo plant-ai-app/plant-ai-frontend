996c12e7655ed9c0193b203527e2f41fa4279441
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-03 12:52:23 -0300

    ♻️ :refactor: Adiciona novas props de estilo

--- src/components/common/container/Container.jsx
+++ src/components/common/container/Container.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import styles from './Container.module.css';
 
-const Container = ({ children, padding }) => {
+const Container = ({ children, padding, alignItems, justifyContent }) => {
     return (
-        <div className={styles.container} style={{padding: padding ? padding : '24% 1.2rem 1.5rem 1.2rem'}}>
+        <div className={styles.container}
+            style={
+                {
+                    padding: padding || '24% 1.2rem 1.5rem 1.2rem',
+                    alignItems: alignItems || 'undefined',
+                    justifyContent: justifyContent || 'undefined'
+                }
+            }>
             {children}
         </div>
     );
