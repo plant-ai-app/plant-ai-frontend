0cfe65572861cda8afd29b7aecdc9cd003c50cfa
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-04 12:50:02 -0300

    ✨ :feat: Adiciona componente de form. RegisterForm

---
+++ src/components/forms/registerForm/RegisterForm.jsx
@@ -0,0 +1,54 @@
+import Input from "../../common/input/Input.jsx";
+import styles from "./RegisterForm.module.css";
+import SubmitButton from "../../common/submitButton/SubmitButton.jsx";
+
+const RegisterForm = ({value, handleChange, onSubmit}) => {
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        onSubmit()
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <Input
+                type="text"
+                text="Digite seu nome"
+                name="nome"
+                placeholder="Nome"
+                handleOnChange={handleChange}
+                value={value.nome ? value.nome : ''}
+                required = {true}
+            />
+            <Input
+                type="email"
+                text="Digite seu email"
+                name="email"
+                placeholder="Email"
+                handleOnChange={handleChange}
+                value={value.email ? value.email : ''}
+                required = {true}
+            />
+            <Input
+                type="password"
+                text="Digite sua senha"
+                name="senha"
+                placeholder="Senha"
+                handleOnChange={handleChange}
+                value={value.senha ? value.senha : ''}
+                required = {true}
+            />
+            <Input
+                type="password"
+                text="Confirme sua senha"
+                name="confirmarSenha"
+                placeholder="Confirmar senha"
+                handleOnChange={handleChange}
+                value={value.confirmarSenha ? value.confirmarSenha : ''}
+                required = {true}
+            />
+            <SubmitButton text="Criar conta"/>
+        </form>
+    );
+};
+
+export default RegisterForm;
