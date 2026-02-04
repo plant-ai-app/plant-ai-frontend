230cedd7bf26b619f1fd866ec1e4b05f7a31f8d1
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-04 12:44:47 -0300

    ♻️ :refactor: Adiciona novas importações

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/pages/home/Home.jsx";
 import Splash from "../components/pages/splash/Splash.jsx";
 import OnBoarding from "../components/pages/onBoarding/OnBoarding.jsx";
+import Login from "../components/pages/login/Login.jsx";
+import Register from "../components/pages/register/Register.jsx";
 
 const AppRoutes = () => {
     return (
@@ -10,6 +12,8 @@ const AppRoutes = () => {
                 <Route path="/" element={<Splash/>} />
                 <Route path="/home" element={<Home/>} />
                 <Route path="/onBoarding" element={<OnBoarding/>} />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/register" element={<Register/>} />
             </Routes>
         </Router>
     );
