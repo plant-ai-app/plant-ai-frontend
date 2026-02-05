3a639f8de45036b84084a6e0037ce32b2136b68b
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-05 14:04:24 -0300

    ♻️ :refactor: Atualiza o componente e seus estilos

--- src/components/forms/loginForm/LoginForm.jsx
+++ src/components/forms/loginForm/LoginForm.jsx
@@ -1,25 +1,36 @@
 import Input from "../../common/input/Input.jsx";
+import SubmitButton from "../../common/submitButton/SubmitButton.jsx";
 import styles from "./LoginForm.module.css";
+import {Link} from "react-router-dom";
 
-const LoginForm = () => {
+const LoginForm = ({value, handleChange, onSubmit}) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit();
+    }
     return (
-        <form>
+        <form className={styles.form_container} onSubmit={handleSubmit}>
             <Input
                 type="email"
                 text="Digite seu email"
                 name="email"
                 placeholder="Email"
                 handleOnChange={handleChange}
-                value={newUser.email ? newUser.email : ''}
+                value={value.email ? value.email : ''}
+                required = {true}
             />
             <Input
                 type="password"
                 text="Digite sua senha"
-                name="password"
-                placeholder="password"
+                name="senha"
+                placeholder="Senha"
                 handleOnChange={handleChange}
-                value={newUser.password ? newUser.password : ''}
+                value={value.senha ? value.senha : ''}
+                required = {true}
             />
+            <span><Link to={'/forgot-password'}>Esqueceu sua senha ?</Link></span>
+
+            <SubmitButton text="Entrar"/>\
         </form>
     );
 };
