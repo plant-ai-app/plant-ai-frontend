1b11d1ddd1110b3813277da0719cb7d3f7176b41
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-04 23:45:04 -0300

    ♻️ :refactor: Melhorias e adição de novas funcionalidades ao componente

--- src/components/pages/register/Register.jsx
+++ src/components/pages/register/Register.jsx
@@ -1,16 +1,29 @@
+import { useState, useEffect} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {useUser} from "../../../hooks/useUser.js"
+
 import Container from "../../common/container/Container.jsx";
 import RegisterForm from "../../forms/registerForm/RegisterForm.jsx";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import Message from "../../layouts/message/Message.jsx";
+import Loading from "../../layouts/loading/Loading.jsx";
 import styles from "./Register.module.css";
 
 const Register = () => {
+    
+    const navigate = useNavigate();
+
+    const {createUser, loading, error} = useUser();
+    const [type, setType] = useState('');
+    const [message, setMessage] = useState();
     const [formData, setFormData] = useState({
         nome: "",
         email: "",
         senha: "",
-        confirmarSenha: ""
+        confirmaSenha: ""
     });
+    const clearForm = () =>{
+        setFormData({nome: '',email: '', senha: '', confirmaSenha: ''})
+    }
 
     const handleChange = (e) => {
         setFormData({
@@ -19,12 +32,40 @@ const Register = () => {
         });
     };
 
-    const handleSubmit = () => {
-        console.log(formData);
+    const handleSubmit = async () => {
+        setMessage('');
+        setType('');
+        try {
+            const data = await createUser(formData);
+
+            setType('success')
+            setMessage(data.message);
+
+            clearForm();
+
+            navigate('/login');
+        } catch (error) {
+            console.log(error)
+            setType('error')
+            setMessage(error.response?.data?.message || "Erro ao cadastrar");
+        }
     };
 
+    useEffect(() => {
+        if (!message) return;
+
+        const timer = setTimeout(() => {
+            setMessage('');
+            setType('');
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, [message]);
+
     return (
-        <Container padding={'18% 1.2rem 2.2rem 1.2rem'}>
+        <Container padding={'16% 1.2rem 2.2rem 1.2rem'}>
+            <Message type={type} msg={message} />
+            {loading && <Loading />}
             <div className={styles.register_container}>
                 <div className={styles.texts_header}>
                     <h1>Vamos criar sua <br /> Conta ✍️</h1>
