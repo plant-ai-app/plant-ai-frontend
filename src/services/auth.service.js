d21eccaf52a470b87acea474cca5803fcbcc63d5
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-05 00:09:34 -0300

    ♻️ :refactor: Atualização no import

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
-import api from "./api";
+import api from "./api.js";
 
-export const login = async (email, senha) =>{
-    const response = await api.post("/usuario/login", {email, senha})
+export const login = async (email, senha) => {
+    const response = await api.post("/usuario/login", { email, senha })
 
     return response.data;
 }
