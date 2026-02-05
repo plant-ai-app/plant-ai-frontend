4a6c3bf465043930b69bfecfcfb75015611f3d6a
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-05 14:03:03 -0300

    ⚡️:perf: Atualiza o componente inteiro e seus estilos.

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,8 +1,72 @@
+import { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useUser } from "../../../hooks/useUser.js";
+
+import Container from "../../common/container/Container.jsx";
+import LoginForm from "../../forms/loginForm/LoginForm.jsx";
+import BackButton from "../../common/backButton/BackButton.jsx";
+import Message from "../../layouts/message/Message.jsx";
+import Loading from "../../layouts/loading/Loading.jsx";
+import styles from "./Login.module.css";
+
 const Login = () => {
+
+    const {loginUser, loading, error} = useUser();
+
+    const navigate = useNavigate();
+
+    const [type, setType] = useState('');
+    const [message, setMessage] = useState('');
+    const [formData, setFormData] = useState({
+        email: "",
+        senha: ""
+    });
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+    const clearForm = () => {
+        setFormData({ email: '', senha: '' })
+    }
+    const handleSubmit = () => {
+        console.log(formData)
+    };
+
+    useEffect(() => {
+        if (!message) return;
+
+        const timer = setTimeout(() => {
+            setMessage('');
+            setType('');
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, [message]);
+
+
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Container padding={'8% 1.2rem 2.2rem 1.2rem'}>
+            <Message type={type} msg={message} />
+            {loading && <Loading />}
+            <div className={styles.login_container}>
+                <BackButton />
+                <div className={styles.texts_header}>
+                    <h1>Bem-vindo de <br /> Volta 👋</h1>
+                    <p>Acesse sua conta e continue cuidando das suas plantas.</p>
+                </div>
+                <div className={styles.form_container}>
+                    <LoginForm
+                        value={formData}
+                        handleChange={handleChange}
+                        onSubmit={handleSubmit}
+                    />
+                </div>
+                <span>Novo por aqui? <Link to={'/register'}>Registre-se</Link></span>
+            </div>
+
+        </Container>
     );
 };
 
