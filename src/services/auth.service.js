024323a23f1c4d32a9ade9760fdc79f4c3d8fcdf
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-06 16:25:16 -0300

    ♻️ :refactor: Melhora a escrita dos métodos.

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,13 +1,16 @@
 import api from "./api.js";
 
-export const login = async (email, senha) => {
-    const response = await api.post("/usuario/login", { email, senha })
-
+export const login = async (userData) => {
+    const response = await api.post("/usuario/login", userData)
     return response.data;
 }
 
-export const forgotPassword = async (email) => {
-    const response = await api.post("/usuario/forgot", { email })
-    console.log(response.data)
+export const forgotPassword = async (userData) => {
+    const response = await api.post("/password/forgot", userData)
+    return response.data;
+}
+
+export const resetPassword = async (userData) => {
+    const response = await api.post("/password/reset", userData)
     return response.data;
 }
