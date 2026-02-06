d41b9c54a9e98d6f5fb0113a7564e7db1517cd2e
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-06 16:17:21 -0300

    ♻️ :refactor: Adiciona novas props ao componente

--- src/components/common/container/Container.jsx
+++ src/components/common/container/Container.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './Container.module.css';
 
-const Container = ({ children, padding, alignItems, justifyContent }) => {
+const Container = ({ children, padding, alignItems, justifyContent, textAlign }) => {
     return (
         <div className={styles.container}
             style={
                 {
                     padding: padding || '24% 1.2rem 1.5rem 1.2rem',
                     alignItems: alignItems || 'undefined',
-                    justifyContent: justifyContent || 'undefined'
+                    justifyContent: justifyContent || 'undefined',
+                    textAlign: textAlign || 'undefined'
                 }
             }>
             {children}
