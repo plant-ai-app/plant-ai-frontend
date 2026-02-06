90bf5b473e5cad6cb7beb870a25b974e37ba9c90
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-06 16:23:50 -0300

    ♻️ :refactor: Refatora arquivo. Retira método não utilizado.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -5,9 +5,3 @@ export const registerUser = async (userData) => {
 
     return response.data;
 }
-
-export const loginUser = async (userData) => {
-    const response = await api.post("/usuario/login", userData)
-
-    return response.data;
-}
