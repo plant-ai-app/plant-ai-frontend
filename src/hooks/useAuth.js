6ca8e0d3a6f8de2c5c3ccb32743404ca842e7fdc
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-02-06 16:19:49 -0300

    ✨ :feat: Adiciona novo arquivo de hook

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import {login as loginUser, forgotPassword as forgotPasswordUser, resetPassword as resetPasswordUser} from "../services/auth.service";
+
+export const useAuth = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    
+    const login = async (userData) => {
+        try {
+            setLoading(true)
+            setError(null)
+
+            const data = await loginUser(userData)
+            return data
+        } catch (error) {
+            setError(error.response.data.erro || 'Erro ao logar usuário')
+            throw error
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const forgotPassword = async (email) => {
+        try {
+            setLoading(true)
+            setError(null)
+
+            const data = await forgotPasswordUser(email)
+            return data
+        } catch (error) {
+            setError(error.response.data.erro || 'Erro ao esquecer senha')
+            throw error
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const resetPassword = async (userData) => {
+        try {
+            setLoading(true)
+            setError(null)
+
+            const data = await resetPasswordUser(userData)
+            return data
+        } catch (error) {
+            setError(error.response.data.erro || 'Erro ao redefinir senha')
+            throw error
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return {login, loading, error, forgotPassword, resetPassword}
+}
