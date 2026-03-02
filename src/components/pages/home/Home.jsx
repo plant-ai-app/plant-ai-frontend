ea495d1170d95493a9fa294fa23441bd4309332b
Bruno Lima Beserra <bruno.beserra2024@gmail.com>
2026-03-02 14:12:43 -0300

    ✨ :feat: Modificação no componente de page. home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,9 +1,21 @@
-import Container from "../../common/container/Container";
+import styles from './Home.module.css';
+import Header from './components/header/Header.jsx';
+import DueToday from './components/doToday/DueToday.jsx';
+import Upcoming from './components/upComing/Upcoming.jsx';
+import MyPlantsList from './components/myPlantsList/MyPlantsList.jsx';
+import BottomNav from '../../layouts/bottomNav/BottomNav.jsx';
+import Container from '../../common/container/Container.jsx';
 
 const Home = () => {
     return (
-        <Container>
-            <h1>Home</h1>
+        <Container padding={'0'}>
+            <div className={styles.scrollArea}>
+                <Header />
+                <DueToday />
+                <Upcoming />
+                <MyPlantsList />
+            </div>
+            <BottomNav />
         </Container>
     );
 };
