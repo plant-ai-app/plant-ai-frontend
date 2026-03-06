eed446e5287791bcd5edc7d2a13646af2b552706
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-06 14:55:41 -0300

    ♻️ :refactor: Adiciona navegação entre telas.

--- src/components/layouts/bottomNav/BottomNav.jsx
+++ src/components/layouts/bottomNav/BottomNav.jsx
@@ -3,16 +3,26 @@ import { BiHomeAlt, BiCalendar } from 'react-icons/bi';
 import { PiPlant } from 'react-icons/pi';
 import { BsPerson } from 'react-icons/bs';
 import { RiFocus3Line } from 'react-icons/ri';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const BottomNav = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
     return (
         <nav className={styles.navContainer}>
             <div className={styles.navBar}>
-                <button className={`${styles.navItem} ${styles.active}`}>
+                <button
+                    className={`${styles.navItem} ${location.pathname === '/home' ? styles.active : ''}`}
+                    onClick={() => navigate('/home')}
+                >
                     <BiHomeAlt className={styles.icon} />
                     <span>Home</span>
                 </button>
-                <button className={styles.navItem}>
+                <button
+                    className={`${styles.navItem} ${location.pathname === '/my-plants' ? styles.active : ''}`}
+                    onClick={() => navigate('/my-plants')}
+                >
                     <PiPlant className={styles.icon} />
                     <span>My Plants</span>
                 </button>
@@ -24,11 +34,17 @@ const BottomNav = () => {
                     <span className={styles.centerLabel}>Identify</span>
                 </div>
 
-                <button className={styles.navItem}>
+                <button
+                    className={`${styles.navItem} ${location.pathname === '/schedule' ? styles.active : ''}`}
+                    onClick={() => navigate('/schedule')}
+                >
                     <BiCalendar className={styles.icon} />
                     <span>Schedule</span>
                 </button>
-                <button className={styles.navItem}>
+                <button
+                    className={`${styles.navItem} ${location.pathname === '/perfil' ? styles.active : ''}`}
+                    onClick={() => navigate('/perfil')}
+                >
                     <BsPerson className={styles.icon} />
                     <span>Profile</span>
                 </button>
