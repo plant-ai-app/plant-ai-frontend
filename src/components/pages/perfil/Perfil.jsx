d2051f5f53583f30d9a5a9c028d6240907a37a86
Bruno Lima Beserra <bruno.beserra2024@gmail.com>
2026-03-12 16:33:57 -0300

    ♻️ :refactor: Criação de design e componentes da página.

--- src/components/pages/perfil/Perfil.jsx
+++ src/components/pages/perfil/Perfil.jsx
@@ -3,11 +3,22 @@ import styles from "./Perfil.module.css";
 
 //components
 import Container from "../../common/container/Container.jsx";
+import ProfileHeader from "./components/ProfileHeader.jsx";
+import ProfileInfo from "./components/ProfileInfo.jsx";
+import ProfileStats from "./components/ProfileStats.jsx";
+import SettingsList from "./components/SettingsList.jsx";
+import AccountList from "./components/AccountList.jsx";
 
 const Perfil = () => {
     return (
         <Container padding={'0'}>
-            <h1>Perfil</h1>
+            <div className={styles.scrollArea}>
+                <ProfileHeader />
+                <ProfileInfo />
+                <ProfileStats />
+                <SettingsList />
+                <AccountList />
+            </div>
         </Container>
     );
 };
