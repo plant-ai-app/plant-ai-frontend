6421a5d201e06829684c660b0548093108291f9d
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-13 21:11:09 -0300

    ♻️ :refactor: Melhorias e correção de bugs.

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,24 +1,45 @@
 import { useState } from "react";
-import {login as loginUser, forgotPassword as forgotPasswordUser, resetPassword as resetPasswordUser} from "../services/auth.service";
+import {
+    login as loginUser,
+    forgotPassword as forgotPasswordUser,
+    resetPassword as resetPasswordUser,
+    changePassword as changePasswordUser,
+} from "../services/auth.service";
 
 export const useAuth = () => {
+    const [token, setToken] = useState(localStorage.getItem("token") || null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-    
+
     const login = async (userData) => {
         try {
             setLoading(true)
             setError(null)
 
             const data = await loginUser(userData)
+            const token = data.usuario.token
+
+            localStorage.setItem("token", token)
+            setToken(token)
+
             return data
         } catch (error) {
-            setError(error.response.data.erro || 'Erro ao logar usuário')
+            setError(error.response?.data?.erro || 'Erro ao logar usuário')
             throw error
         } finally {
             setLoading(false)
         }
     }
+
+    const logout = () => {
+        localStorage.removeItem("token")
+        setToken(null)
+    }
+
+    const isAuthenticated = () => {
+        return !!token
+    }
+
 
     const forgotPassword = async (userData) => {
         try {
@@ -50,5 +71,29 @@ export const useAuth = () => {
         }
     }
 
-    return {login, loading, error, forgotPassword, resetPassword}
+    const changePassword = async (userData) => {
+        try {
+            setLoading(true)
+            setError(null)
+
+            const data = await changePasswordUser(userData)
+            return data
+        } catch (error) {
+            setError(error.response.data.erro || 'Erro ao alterar senha')
+            throw error
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return {
+        login,
+        loading,
+        error,
+        forgotPassword,
+        resetPassword,
+        changePassword,
+        isAuthenticated,
+        logout
+    }
 }
