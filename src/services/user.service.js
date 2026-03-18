574e507604684a9184baeb438dbfcb81c5826f0e
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-18 17:35:13 -0300

    ✨ :feat: Adiciona novos métodos.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,3 +4,14 @@ export const registerUser = async (userData) => {
     const response = await api.post("/usuario", userData)
     return response.data;
 }
+
+export const updateUser = async (userData) => {
+    const response = await api.patch("/usuario", userData)
+    console.log(response.data)
+    return response.data;
+}
+
+export const getUser = async () => {
+    const response = await api.get(`/usuario/me`)
+    return response.data;
+}
