eef736a4aad38680bf2ca85bab33b2458a3e9e2e
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-12 23:53:10 -0300

    ♻️ :refactor: Mudanças nas rotas.

--- src/services/plant.service.js
+++ src/services/plant.service.js
@@ -1,31 +1,31 @@
 import api from "./api";
 
 export const getPlants = async () => {
-    const response = await api.get("/plant");
-    return response.data;
+    const response = await api.get("/plants/user");
+    return response.data.plantas;
 }
 
 export const getPlantById = async (id) => {
-    const response = await api.get(`/plant/${id}`);
+    const response = await api.get(`/plants/${id}`);
     return response.data;
 }
 
 export const getPlantByName = async (name) => {
-    const response = await api.get(`/plant/name/${name}`);
+    const response = await api.get(`/plants/name/${name}`);
     return response.data;
 }
 
 export const createPlant = async (plant) => {
-    const response = await api.post("/plant", plant);
+    const response = await api.post("/plants", plant);
     return response.data;
 }
 
 export const updatePlant = async (id, plant) => {
-    const response = await api.put(`/plant/${id}`, plant);
+    const response = await api.put(`/plants/${id}`, plant);
     return response.data;
 }
 
 export const deletePlant = async (id) => {
-    const response = await api.delete(`/plant/${id}`);
+    const response = await api.delete(`/plants/${id}`);
     return response.data;
 }
