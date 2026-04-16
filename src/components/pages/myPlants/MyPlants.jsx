11b32b7e9caf40abffc83f5f2ba16cdd7236d118
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-16 12:23:06 -0300

    ♻️ :refactor: Refatoração no componente.

--- src/components/pages/myPlants/MyPlants.jsx
+++ src/components/pages/myPlants/MyPlants.jsx
@@ -8,17 +8,10 @@ import Header from "./components/Header.jsx";
 import SearchBar from "./components/SearchBar.jsx";
 import CategoryTabs from "./components/CategoryTabs.jsx";
 import PlantList from "./components/PlantList.jsx";
-import AddButton from "./components/AddButton.jsx";
 
 const MyPlants = () => {
     const [filterCategory, setFilterCategory] = useState("Todas");
     const [searchQuery, setSearchQuery] = useState("");
-    // const navigate = useNavigate();
-
-    // const handleAddPlant = () => {
-    //     Mock function for adding a plant
-    //     alert("Add button clicked");
-    // };
 
     return (
         <Container padding={"0"}>
@@ -28,7 +21,6 @@ const MyPlants = () => {
                 <CategoryTabs onSelectCategory={setFilterCategory} />
                 <PlantList filterCategory={filterCategory} searchQuery={searchQuery} />
             </div>
-            {/* <AddButton onClick={() => navigate("/scan")} /> */}
         </Container>
     );
 };
