c8d23366cde9465b925f411d7cbcba719eb718e4
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-16 14:24:07 -0300

    ✨ :feat: Adiciona novo componente e seus estilos.

---
+++ src/components/pages/plantSettings/PlantSettings.jsx
@@ -0,0 +1,11 @@
+import styles from "./PlantSettings.module.css";
+
+const PlantSettings = () => {
+    return (
+        <div>
+            <h1>Plant Settings</h1>
+        </div>
+    );
+};
+
+export default PlantSettings;
