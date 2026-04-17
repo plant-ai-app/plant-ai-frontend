57755f2f3143d73c61dd8517f73cc79f2cd7dae9
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-17 11:09:53 -0300

    ♻️ :refactor: Adiciona novas props.

--- src/components/layouts/loading/Loading.jsx
+++ src/components/layouts/loading/Loading.jsx
@@ -1,10 +1,12 @@
 import styles from './Loading.module.css';
 import img from './img_loading.svg';
 
-function Loading(){
+function Loading({ inline = false, size }){
+    const imageStyle = size ? { width: size, height: size } : {};
+    
     return(
-        <div className={styles.loading_container}>
-            <img src={img} alt="loading image" />
+        <div className={inline ? styles.loading_inline : styles.loading_container}>
+            <img src={img} alt="loading image" style={imageStyle} />
         </div>
     )
 }
