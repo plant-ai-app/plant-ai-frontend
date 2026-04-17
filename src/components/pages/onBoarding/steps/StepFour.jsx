73590fcc42ac357fca57842d5e251a95db4f5f8a
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-17 11:15:12 -0300

    ♻️ :refactor: Mudança no conteúdo da página.

--- src/components/pages/onBoarding/steps/StepFour.jsx
+++ src/components/pages/onBoarding/steps/StepFour.jsx
@@ -5,22 +5,22 @@ import { useNavigate } from 'react-router-dom';
 const StepFour = () => {
 
     const navigate = useNavigate();
-    
+
     return (
         <div className={styles.step_container}>
             <div className={styles.img}>
                 <img src={img} alt="" />
             </div>
             <div className={styles.texts}>
-                <h1>Organize e acompanhe seus cuidados</h1>
+                <h1>Comece sua coleção hoje</h1>
                 <p>
-                   Dê o primeiro passo no cuidado das suas plantas.
+                   Tudo o que suas plantas precisam em um só lugar.
                 </p>
             </div>
             <div className={styles.buttons}>
                 <div className={styles.last_step_buttons}>
-                    <button onClick={() => {navigate('/register')}}>Criar</button>
-                    <button onClick={() => {navigate('/login')}}>Entrar</button>
+                    <button onClick={() => { navigate('/register') }}>Criar</button>
+                    <button onClick={() => { navigate('/login') }}>Entrar</button>
                 </div>
             </div>
         </div>
