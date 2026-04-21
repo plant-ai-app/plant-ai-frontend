4eb58038045e9d425c5065b0b94af20b18b3e817
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-21 18:02:31 -0300

    ✨ :feat: Adiciona novo componenete mais estilos - InstructionsInput.jsx  e InstructionsInput.module.css

---
+++ src/components/care/components/InstructionsInput.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './InstructionsInput.module.css';
+
+const InstructionsInput = ({ value, onChange }) => {
+    return (
+        <div>
+            <h4 className={styles.sectionTitle}>Instruções</h4>
+            <div className={styles.textAreaBox}>
+                <textarea 
+                    className={styles.instructionsInput}
+                    placeholder="Instruções especiais (ex: Usar 200ml de água filtrada)"
+                    value={value}
+                    onChange={(e) => onChange(e.target.value)}
+                ></textarea>
+            </div>
+        </div>
+    );
+};
+
+export default InstructionsInput;
