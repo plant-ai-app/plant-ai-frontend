27c34c6cbd19d92a56ee15624899ac741d23383f
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-21 00:46:52 -0300

    ♻️ :refactor: Melhorias na lógica do componente.

--- src/components/pages/perfil/components/ProfileHeader.jsx
+++ src/components/pages/perfil/components/ProfileHeader.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+//styles and icons
 import styles from './ProfileHeader.module.css';
-import { BsArrowLeft, BsGearFill } from 'react-icons/bs';
+import { BsGearFill } from 'react-icons/bs';
+//hooks
 import { useNavigate } from 'react-router-dom';
+//components
+import BackButton from '../../../common/backButton/BackButton.jsx';
 
 const ProfileHeader = () => {
     const navigate = useNavigate();
 
     return (
         <div className={styles.headerContainer}>
-            <button className={styles.iconButton} onClick={() => navigate(-1)} aria-label="Go back">
-                <BsArrowLeft />
-            </button>
+            <BackButton 
+                width="40px"
+                height="40px"
+                borderRadius="50%"
+                backgroundColor="transparent"
+                color="#1a1a1a"
+                onClick={()=> navigate(-1)}
+            />
             <h2 className={styles.title}>Perfil</h2>
             <button onClick={() => alert('Settings')} className={styles.iconButton} aria-label="Settings">
                 <BsGearFill />
