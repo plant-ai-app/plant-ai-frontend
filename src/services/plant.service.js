9fd52acb5ab2d502e5a8ebde12496a0d2101d0b3
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-24 12:41:43 -0300

    ♻️ :refactor:  Mudanças nos endpoints.

--- src/services/plant.service.js
+++ src/services/plant.service.js
@@ -6,7 +6,7 @@ export const getPlants = async () => {
 }
 
 export const getPlantById = async (id) => {
-    const response = await api.get(`/plants/${id}`);
+    const response = await api.get(`/plant/${id}`);
     return response.data.planta;
 }
 
@@ -21,11 +21,11 @@ export const createPlant = async (plant) => {
 }
 
 export const updatePlant = async (id, plant) => {
-    const response = await api.put(`/plants/${id}`, plant);
+    const response = await api.put(`/plant/${id}`, plant);
     return response.data;
 }
 
 export const deletePlant = async (id) => {
-    const response = await api.delete(`/plants/${id}`);
+    const response = await api.delete(`/plant/${id}`);
     return response.data;
 }
