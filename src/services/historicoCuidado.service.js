0a5fa0ddb7d9da956f8c3c963462d76c8b9cdbff
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-05-05 13:58:24 -0300

    ♻️ :refactor: Adiciona novo método.

--- src/services/historicoCuidado.service.js
+++ src/services/historicoCuidado.service.js
@@ -4,3 +4,8 @@ export const createHistoricoCuidado = async (data) => {
     const response = await api.post("/historico-cuidado", data);
     return response.data;
 }
+
+export const getAllHistoricoCuidado = async () => {
+    const response = await api.get("/historico-cuidado");
+    return response.data;
+}
