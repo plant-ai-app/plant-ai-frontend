8c4260ec84ac6e73262fc13b60ed543d316d743d
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-05-07 22:19:08 -0300

    ♻️ :refactor: Melhorias no componente.

--- src/components/pages/schedule/components/Header.jsx
+++ src/components/pages/schedule/components/Header.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import styles from './Header.module.css';
-import { BsCalendar3, BsPlus } from 'react-icons/bs';
+import { BsCalendar3 } from 'react-icons/bs';
 
 const Header = () => {
     return (
         <div className={styles.headerContainer}>
-            <h1 className={styles.title}>Agenda</h1>
+            <h1 className={styles.title}>Cuidados Agendados</h1>
             <div className={styles.actions}>
                 <button className={styles.iconButton} aria-label="Calendário">
                     <BsCalendar3 />
